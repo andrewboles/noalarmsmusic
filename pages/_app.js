2e592ff0d3fd3a0459e118e1fc0ace751b046cb3
andrewboles <andrewboles@me.com>
2022-12-22 11:39:54 -0800

    klaviyo form

--- pages/_app.js
+++ pages/_app.js
@@ -10,12 +10,17 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>no alarms</title>
         <link rel="shortcut icon" href="/favicon.png" />
-        <script async type="text/javascript" src="https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=VE8yCQ" />
       </Head>
       <link
         rel="stylesheet"
         href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.14.0/devicon.min.css"
       />
+      <script
+        async
+        type="text/javascript"
+        src="https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=VE8yCQ"
+      />
+
       <Component {...pageProps} />
     </>
   );
